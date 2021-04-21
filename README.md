e410cedbe3a3a31c6b3bda1f0ec666af281076c3
kishore5242 <kishore5242@gmail.com>
2021-04-21 17:09:28 +0530

    init - thanks to mkyong

---
+++ README.md
@@ -0,0 +1,11 @@
+# Smart Backup
+
+Backup your files in a smart way
+
+## 1. How to start
+```
+$ git clone [https://github.com/kishore5242/smart-backup.git](https://github.com/kishore5242/smart-backup.git)
+$ cd smart-backup
+$ mvn spring-boot:run
+
+```
---
+++ src/main/java/com/kishore/sb/SmartBackupApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBackupApplication.class, args);
+    }
+
+}
