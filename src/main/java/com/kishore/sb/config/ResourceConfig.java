c03f2402555d820ec7d784e81eb44e0196df929d
kishore5242 <kishore5242@gmail.com>
2021-04-21 19:15:26 +0530

    Image preview sample

---
+++ src/main/java/com/kishore/sb/config/ResourceConfig.java
@@ -0,0 +1,14 @@
+package com.kishore.sb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/thumbnails/**").addResourceLocations("file:thumbnails/");
+    }
+}
---
+++ src/main/java/com/kishore/sb/model/FolderSelection.java
@@ -0,0 +1,24 @@
+package com.kishore.sb.model;
+
+public class FolderSelection {
+
+	private String sourceFolder;
+	private String backupFolder;
+
+	public String getSourceFolder() {
+		return sourceFolder;
+	}
+
+	public void setSourceFolder(String sourceFolder) {
+		this.sourceFolder = sourceFolder;
+	}
+
+	public String getBackupFolder() {
+		return backupFolder;
+	}
+
+	public void setBackupFolder(String backupFolder) {
+		this.backupFolder = backupFolder;
+	}
+
+}
